6cd64f8272d3e26915c5340413805f57d88ca607
Juvenal Ortega <jorte057@wch136-15.cs.ucr.edu>
2020-02-21 22:15:18 -0800

    completed part 1

--- source/main.c
+++ source/main.c
@@ -69,31 +69,38 @@ unsigned char led0_output = 0x00;
 unsigned char led1_output = 0x00;
 unsigned char pause = 0;
 
-enum pauseButtonSM_States {pauseButton_wait, pauseButton_press, pauseButton_release};
+enum getButton {getButtonSystem};
 
 int pauseButtonSMTick (int state) {
-	unsigned char press = ~PINA & 0x01;
-
 	switch (state) {
-		case pauseButton_wait:
-			state = press = 0x01? pauseButton_press: pauseButton_wait; break;
-		case pauseButton_press:
-			state = pauseButton_release; break;
-		case pauseButton_release:
-			state = press == 0x00? pauseButton_wait: pauseButton_press; break;
-		default: state = pauseButton_wait; break;
-	}
-	switch (state) {
-		case pauseButton_wait: break;
-		case pauseButton_press:
-			pause = (pause == 0) ? 1 : 0;
-			break;
-		case pauseButton_release: break;
+		case getButtonSystem : {
+		unsigned char x;
+		x = GetKeypadKey();
+        	switch(x) {
+                case '1' : PORTB = 0x01; break;
+                case '2' : PORTB = 0x02; break;
+                case '3' : PORTB = 0x03; break;
+                case '4' : PORTB = 0x04; break;
+                case '5' : PORTB = 0x05; break;
+                case '6' : PORTB = 0x06; break;
+                case '7' : PORTB = 0x07; break;
+                case '8' : PORTB = 0x08; break;
+                case '9' : PORTB = 0x09; break;
+                case 'A' : PORTB = 0x0A; break;
+                case 'B' : PORTB = 0x0B; break;
+                case 'C' : PORTB = 0x0C; break;
+                case 'D' : PORTB = 0x0D; break;
+                case '*' : PORTB = 0x0E; break;
+                case '0' : PORTB = 0x00; break;
+                case '#' : PORTB = 0x0F; break;         
+                default : PORTB = 0x1B; break;
+        	}
+		}
 	}
 	return state;
 }
 
-enum toggleLED0_States {toggleLED0_wait, toggleLED0_blink };
+/*enum toggleLED0_States {toggleLED0_wait, toggleLED0_blink };
 
 int toggleLED0SMTick(int state) {
 	switch (state) {
@@ -143,48 +150,24 @@ int displaySMTick (int state) {
 		}
 		PORTB = output;
 		return state;
-}
-
-/*unsigned char GetSingleButton(unsigned char row, unsigned char col){
-	//set b3..b0 outputs to 1 except the bit at positino 'col',
-	//ensuring b7..b4 are not modified
-	B = (B | 0x0F) & ~(1 << col);
-
-	//now read the input pin at position 'row'. If the button at 
-	//row/col is pressed, it will go from pull-up state (1) to 0
-	return ((A & (1 << row)) == 0 ? 1 : 0;
-}
-
-unsigned char ScanKeypad(){
-	unsigned char i, j;
-	for (i = 0; i < 4; i++){
-		for (j = 0; j < 4; j++){
-			if (GetSingleButton(i,j)){
-				return (i * 4) + j + 1;
-			}
-		}
-	}
-	return 0;
-}*/
-	
+}*/	
 
 int main(void) {
-	//unsigned char x;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRA = 0x00; PORTA = 0xFF;
-	//DDRC = 0xF0; PORTC = 0x0F;
+	DDRC = 0xF0; PORTC = 0x0F;
 	//LCD_init();
 	
-	static task task1, task2, task3, task4;
-        task *tasks[] = { &task1, &task2, &task3, &task4 };
+	static task task1; //task2, task3, task4;
+        task *tasks[] = { &task1};//, &task2, &task3, &task4 };
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 	
-	task1.state = pauseButton_wait;
+	task1.state = getButtonSystem;
 	task1.period = 50;
 	task1.elapsedTime = task1.period;
 	task1.TickFct = &pauseButtonSMTick;
 
-	task2.state = toggleLED0_wait;
+	/*task2.state = toggleLED0_wait;
         task2.period = 500;
         task2.elapsedTime = task2.period;
         task2.TickFct = &toggleLED0SMTick;
@@ -197,13 +180,14 @@ int main(void) {
 	task4.state = display_display;
         task4.period = 10;
         task4.elapsedTime = task4.period;
-        task4.TickFct = &displaySMTick;
+        task4.TickFct = &displaySMTick;*/
 	
 	TimerSet(10);
 	TimerOn();
 	
 	unsigned short i;
     while (1) {
+
 	for (i = 0; i < numTasks; i++ ) {
 		if (tasks[i]->elapsedTime == tasks[i]->period){
 			tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
@@ -214,27 +198,6 @@ int main(void) {
 	while(!TimerFlag);
 	TimerFlag = 0;
 	
-
-	/*x = GetKeypadKey();
-	switch(x) {
-		case '1' : PORTB = 0x01; break;
-		case '2' : PORTB = 0x02; break;
-		case '3' : PORTB = 0x03; break;
-                case '4' : PORTB = 0x04; break;
-                case '5' : PORTB = 0x05; break;
-		case '6' : PORTB = 0x06; break;
-                case '7' : PORTB = 0x07; break;
-                case '8' : PORTB = 0x08; break;
-                case '9' : PORTB = 0x09; break;
-                case 'A' : PORTB = 0x0A; break;
-                case 'B' : PORTB = 0x0B; break;
-		case 'C' : PORTB = 0x0C; break;
-                case 'D' : PORTB = 0x0D; break;
-                case '*' : PORTB = 0x0E; break;
-                case '0' : PORTB = 0x00; break;
-		case '#' : PORTB = 0x0F; break;		
-		default : PORTB = 0x1B; break;
-	}*/
     }
     return 1;
 }
